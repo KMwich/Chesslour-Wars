2fefdfa57f92e619456b4d65e989ac984514769b
Kaiwit Maneewong <Kaiwit.maneewong@gmail.com>
2018-01-07 21:54:57 +0700

    change direction and cell with image

---
+++ Assets/Resources/script/HexCoordinates.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates {
+    [SerializeField]
+    private int x, y;
+
+    public int X {
+        get {
+            return x;
+        }
+    }
+
+    public int Y {
+        get {
+            return y;
+        }
+    }
+
+    public int Z {
+        get {
+            return -X - Y;
+        }
+    }
+
+    public HexCoordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates(int x, int y) {
+        return new HexCoordinates(x, y - x / 2);
+    }
+
+    public override string ToString() {
+        return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
+    }
+
+    public string ToStringOnSeparateLines() {
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
+    }
+
+    public static HexCoordinates FromPosition(Vector3 position) {
+        float z = position.z / (HexMetrics.innerRadius * 2f);
+        float y = -z;
+
+        float offset = position.x / (HexMetrics.outerRadius * 3f);
+        z -= offset;
+        y -= offset;
+
+        int iX = Mathf.RoundToInt(-z - y);
+        int iY = Mathf.RoundToInt(y);
+        int iZ = Mathf.RoundToInt(z);
+
+        if (iX + iY + iZ != 0) {
+            float dX = Mathf.Abs(-z - y - iX);
+            float dY = Mathf.Abs(y - iY);
+            float dZ = Mathf.Abs(z - iZ);
+
+            if (dZ > dY && dZ > dX) {
+                iZ = -iY - iX;
+            } else if (dX > dY) {
+                iX = -iZ - iY;
+            }
+        }
+
+        Debug.Log(iX + "," + iZ + "," + iY);
+
+        return new HexCoordinates(iX, iZ);
+    }
+}
---
+++ Assets/Resources/script/HexGrid.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    public int width = 6;
+    public int height = 6;
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
+    public HexCell cellPrefab;
+
+    HexCell[] cells;
+
+    public Text cellLabelPrefab;
+
+    HexMesh hexMesh;
+
+    public Sprite[] maps;
+
+    void Awake(){
+        
+        hexMesh = GetComponentInChildren<HexMesh>();
+        maps = Resources.LoadAll<Sprite>("sprite/map");
+
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+
+    }
+
+    void Start(){
+        hexMesh.Triangulate(cells);
+    }
+
+    void CreateCell(int x, int z, int i){
+
+        Vector3 position;
+        position.x = x * (HexMetrics.outerRadius * 1.5f);
+        position.y = - z - ((float) i % 4 / 4f );
+        position.z = (z + x * 0.5f - x / 2) * (HexMetrics.innerRadius * 2f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+        cell.GetComponent<SpriteRenderer>().sprite = maps[i % 4];
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            HandleInput();
+        }
+    }
+
+    void HandleInput () {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            TouchCell(hit.point);
+        }
+    }
+
+    void TouchCell (Vector3 position) {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + (coordinates.Y + coordinates.X / 2) * width;
+        HexCell cell = cells[index];
+        cell.color = touchedColor;
+        hexMesh.Triangulate(cells);
+    }
+
+}
