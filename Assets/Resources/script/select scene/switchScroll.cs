9ad4bade42554e631ff2b4ca647372d49d26eb00
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2018-01-13 20:04:02 +0700

    1132018
    can switch planel and update stat text

---
+++ Assets/Resources/script/select scene/switchScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchScroll : MonoBehaviour {
+
+    private List<GameObject> planels;
+    private int viewIndex;
+
+	// Use this for initialization
+	void Start () {
+        planels = new List<GameObject>();
+        foreach(Transform t in transform)
+        {
+            planels.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+        planels[0].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+
+    public void select(int index)
+    {
+        if (index == viewIndex)
+            return;
+        if (index < 0 || index >= planels.Count)
+            return;
+        planels[viewIndex].SetActive(false);
+        viewIndex = index;
+        planels[viewIndex].SetActive(true);
+    }
+}
