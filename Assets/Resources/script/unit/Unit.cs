10cdd6430ccef20ce55b74ae06fdfa7b718d86dc
Kaiwit Maneewong <Kaiwit.maneewong@gmail.com>
2018-01-26 19:40:04 +0700

    commit

--- Assets/Resources/script/unit/Unit.cs
+++ Assets/Resources/script/unit/Unit.cs
@@ -7,21 +7,14 @@ public class Unit : Photon.MonoBehaviour {
  //   public HexGrid map;
     int attackRange = 1;
     bool move = false;
+    private Vector3 TargetPosition;
+    private Quaternion TargetRotation;
     private PhotonView PhotonView;
+    public bool canDrag = true;
 
     // Use this for initialization
     void Awake () {
-        int x = Random.Range(0, 6);
-        int z = Random.Range(0, 6);
-
         PhotonView = GetComponent<PhotonView>();
-
-        Vector3 position;
-        position.x = x;
-        position.y = 0;
-        position.z = z;
-
-        setUnitPosition(position);
     }
 
     // Update is called once per frame
@@ -51,12 +44,30 @@ public class Unit : Photon.MonoBehaviour {
         
     }
 
-    void setUnitPosition(Vector3 position) {
+    public void setUnitPosition(Vector3 position) {
         coordinate = HexCoordinates.FromOffsetCoordinates((int)position.x, (int)position.z);
         position.x = position.x * (HexMetrics.outerRadius * 1.5f);
         position.y = 2;
         position.z = (position.z + position.x * 0.5f - position.x / 2) * (HexMetrics.innerRadius * 2f);
         transform.localPosition = position;
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            TargetPosition = (Vector3)stream.ReceiveNext();
+            TargetRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    public void asd() {
+        Debug.Log( this.GetComponentInParent<UnitBar>().name);
     }
 
 }
---
+++ Assets/Resources/script/unit/UnitBar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class UnitBar : MonoBehaviour {
+    public HexGrid _hexGrid;
+
+    public Unit unitPrefab;
+    Unit[] units;
+    Unit selectUnit;
+
+    private void Awake() {
+        units = new Unit[5];
+
+        for (int i = 0; i < units.Length; i++) {
+            createUnit(i);
+        }
+    }
+
+    private void createUnit(int i) {
+        Unit unit = units[i] = Instantiate<Unit>(unitPrefab);
+
+        Vector3 position;
+        position.x = i;
+        position.y = 2;
+        position.z = -1;
+
+        unit.setUnitPosition(position);
+        unit.transform.SetParent(transform);
+    }
+
+    public void setSelectUnit() {
+        selectUnit = this.gameObject.GetComponent<Unit>();
+    }
+
+    private void Update() {
+        if (selectUnit != null)
+            selectUnit.transform.localPosition = Input.mousePosition;
+    }
+}
