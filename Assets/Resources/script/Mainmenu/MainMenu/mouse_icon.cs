2bf83099890e7961c1dc3d7e3b16da8ac0085b50
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2018-02-13 17:49:40 +0700

    21320178
    add gui phase 3

---
+++ Assets/Resources/script/Mainmenu/MainMenu/mouse_icon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class mouse_icon : MonoBehaviour {
+
+    public Texture2D mouse_img;
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(mouse_img,Vector2.zero,CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
